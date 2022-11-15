0cc4e9cce1b7669411c6de624e7c2f2a32e75596
Vlad Yelahin <vyelahin@varteq.com>
2022-11-15 16:33:56 +0200

    using sharp instead of node-canvas

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -1,9 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { decode } from 'blurhash';
+import sharp from 'sharp';
 
 import { DEFAULT_LIMIT, DEFAULT_PAGE } from '@utils/const';
-import { blurhashToDataUrl } from '@utils/index';
 import { IImage, IImagesResponse } from 'interfaces';
 import unsplash from 'services/unsplash';
+
+/**
+ * Convert base84 encoded blurhash to base64 image.
+ * NOTE: this can't be moved outside of /api folder since webpack will try to include it
+ * into client build, and sharp can't be used client-side
+ */
+export const blurhashToDataUrl = async (
+  hash: string,
+  width: number = 100,
+  height: number = 100,
+) => {
+  const u8 = decode(hash, width, height);
+  const blurDataUrlBuffer = await sharp(u8, {
+    raw: {
+      channels: 4,
+      width,
+      height,
+    },
+  })
+    .jpeg({
+      overshootDeringing: true,
+      quality: 40,
+    })
+    .toBuffer();
+
+  return `data:image/jpeg;base64,${blurDataUrlBuffer.toString('base64')}`;
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -31,20 +59,22 @@ export default async function handler(
           throw new Error();
         }
 
-        const { results, total } = response;
+        const { results } = response;
 
-        const parsedImages: IImage[] = results.map((img) => ({
-          id: img.id,
-          description: img.alt_description || 'No description 😒',
-          url: img.urls.regular,
-          width: img.width,
-          height: img.height,
-          blurHash: blurhashToDataUrl(
-            img.blur_hash!,
-            Math.round(img.width / 100),
-            Math.round(img.height / 100),
-          ),
-        }));
+        const parsedImages: IImage[] = await Promise.all(
+          results.map(async (img) => ({
+            id: img.id,
+            description: img.alt_description || 'No description 😒',
+            url: img.urls.regular,
+            width: img.width,
+            height: img.height,
+            blurHash: await blurhashToDataUrl(
+              img.blur_hash!,
+              Math.round(img.width / 100),
+              Math.round(img.height / 100),
+            ),
+          })),
+        );
 
         res.status(200).send({ images: parsedImages });
       } catch (err) {
